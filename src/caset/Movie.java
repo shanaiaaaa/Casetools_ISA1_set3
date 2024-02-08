07bfefc5f0dd0c230e86824c71b2a2afdbf17e64
shanaia fernandes <shanaia0213@gmail.com>
2024-02-08 09:24:19 +0530

    first commit

---
+++ src/caset/Movie.java
@@ -0,0 +1,14 @@
+package caset;
+
+public class Movie {
+	private String title;
+	private String director;
+	public Movie(String title, String director) {
+		this.title=title;
+		this.director=director;
+	}
+	public String getTitle() {
+		return this.title;
+	}
+
+}
